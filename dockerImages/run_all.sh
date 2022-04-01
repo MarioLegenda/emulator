e10911cc7c54c2dc1531c5b853e6f006b6c3437f
Mario Škrlec <mario@prototyp.digital>
2022-04-01 18:27:02 +0200

    Added csharp_mono to emulator run_all script

--- dockerImages/run_all.sh
+++ dockerImages/run_all.sh
@@ -13,3 +13,4 @@ cd "$dir/rust" && /usr/bin/docker image build -t rust:rust .
 cd "$dir/haskell" && /usr/bin/docker image build -t haskell:haskell .
 cd "$dir/c" && /usr/bin/docker image build -t c:c .
 cd "$dir/c++" && /usr/bin/docker image build -t c-plus:c-plus .
+cd "$dir/c_sharp_mono" && /usr/bin/docker image build -t c_sharp_mono:c_sharp_mono .
