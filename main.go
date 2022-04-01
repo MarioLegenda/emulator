303e318b763bce5031949036e9d913b50ba1dee3
Mario Škrlec <mario@prototyp.digital>
2022-04-01 11:36:35 +0200

    Reverted to previous

--- main.go
+++ main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os/user"
-)
-
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	username := user.Username
-
-	fmt.Printf("Username: %s\n", username)
-
 	App()
 }
