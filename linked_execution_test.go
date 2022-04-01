619185d6500020112923be5b147196e692404967
Mario Škrlec <mario@prototyp.digital>
2022-04-01 08:28:56 +0200

    Added tests for nodeJS ESm

--- linked_execution_test.go
+++ linked_execution_test.go
@@ -507,4 +507,121 @@ module.exports = {
 		gomega.Expect(result.Success).Should(gomega.BeTrue())
 		gomega.Expect(result.Result).Should(gomega.Equal("rootDirectoryFile2\nsubDirFile\nrootDirectoryFile1\n"))
 	})
+
+	GinkgoIt("Should run a linked code block execution as a session in a NodeJS ESM environment", func() {
+		testPrepare()
+		defer testCleanup()
+
+		activeSession := testCreateAccount()
+		link := testCreateBlog(activeSession)
+		pageUuid := link["page"].(map[string]interface{})["uuid"].(string)
+		blogUuid := link["blog"].(map[string]interface{})["uuid"].(string)
+
+		cp := testCreateCodeProject(activeSession, uuid.New().String(), runner.NodeEsm)
+		cpUuid := cp["uuid"].(string)
+		cb := testCreateCodeBlock(pageUuid, activeSession)
+		testUpdateCodeBlock(activeSession, pageUuid, cb["uuid"].(string), `
+import './rootDirectoryFile1.mjs';
+`)
+		testLinkCodeProject(activeSession, cp["uuid"].(string), pageUuid, cb["uuid"].(string), blogUuid)
+
+		var rootDirectory map[string]interface{}
+		s, err := json.Marshal(cp["rootDirectory"])
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(json.Unmarshal(s, &rootDirectory)).Should(gomega.BeNil())
+
+		rootDirectoryFile1 := testCreateFile(activeSession, true, rootDirectory["uuid"].(string), cpUuid, "rootDirectoryFile1.mjs")
+		testUpdateFileContent(activeSession, cpUuid, rootDirectoryFile1["uuid"].(string), fmt.Sprintf(`
+import {execute} from './rootDirectoryFile2.mjs';
+import {subDirDirFileExecute} from './subDir/subSubDir/subSubDirFile.mjs';
+
+execute();
+
+console.log('rootDirectoryFile1');
+`))
+
+		rootDirectoryFile2 := testCreateFile(activeSession, true, rootDirectory["uuid"].(string), cpUuid, "rootDirectoryFile2.mjs")
+		testUpdateFileContent(activeSession, cpUuid, rootDirectoryFile2["uuid"].(string), `
+import {subDirFileExecute} from './subDir/subDirFile.mjs';
+
+function execute() {
+    console.log('rootDirectoryFile2');
+
+    subDirFileExecute();
+}
+
+export { execute };
+`)
+
+		rootDirectorySubDir := testCreateFile(activeSession, false, rootDirectory["uuid"].(string), cpUuid, "subDir")
+		subDirFile := testCreateFile(activeSession, true, rootDirectorySubDir["uuid"].(string), cpUuid, "subDirFile.mjs")
+		testUpdateFileContent(activeSession, cpUuid, subDirFile["uuid"].(string), `
+function subDirFileExecute() {
+    console.log('subDirFile');
+}
+
+export { subDirFileExecute };
+`)
+
+		subDir := testCreateFile(activeSession, false, rootDirectorySubDir["uuid"].(string), cpUuid, "subSubDir")
+		subDirSubFile := testCreateFile(activeSession, true, subDir["uuid"].(string), cpUuid, "subSubDirFile.mjs")
+		testUpdateFileContent(activeSession, cpUuid, subDirSubFile["uuid"].(string), `
+function subDirDirFileExecute() {
+    console.log('subSubDirFile');
+}
+
+export { subDirDirFileExecute };
+`)
+		sessionUuid := testCreateLinkedSession(activeSession, pageUuid, cb["uuid"].(string))
+
+		bm := map[string]interface{}{
+			"uuid": sessionUuid,
+		}
+
+		body, err := json.Marshal(bm)
+
+		gomega.Expect(err).To(gomega.BeNil())
+
+		req, err := http.NewRequest("POST", "/api/environment-emulator/execute/linked-code-block", bytes.NewReader(body))
+
+		if err != nil {
+			ginkgo.Fail(err.Error())
+
+			return
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(executeLinkedProjectHandler)
+
+		handler.ServeHTTP(rr, req)
+
+		b := rr.Body.Bytes()
+
+		var apiResponse httpUtil.ApiResponse
+		err = json.Unmarshal(b, &apiResponse)
+
+		gomega.Expect(err).To(gomega.BeNil())
+
+		gomega.Expect(rr.Code).To(gomega.Equal(http.StatusOK))
+		gomega.Expect(rr.Body).To(gomega.Not(gomega.BeNil()))
+
+		gomega.Expect(apiResponse.Method).To(gomega.Equal("POST"))
+		gomega.Expect(apiResponse.Type).To(gomega.Equal(staticTypes.RESPONSE_RESOURCE))
+		gomega.Expect(apiResponse.Message).To(gomega.Equal("Emulator run result"))
+		gomega.Expect(apiResponse.MasterCode).To(gomega.Equal(0))
+		gomega.Expect(apiResponse.Code).To(gomega.Equal(0))
+		gomega.Expect(apiResponse.Status).To(gomega.Equal(http.StatusOK))
+		gomega.Expect(apiResponse.Pagination).To(gomega.BeNil())
+
+		b, err = json.Marshal(apiResponse.Data)
+
+		gomega.Expect(err).To(gomega.BeNil())
+
+		var result runner.SingleFileRunResult
+		gomega.Expect(json.Unmarshal(b, &result)).To(gomega.BeNil())
+
+		gomega.Expect(result.Timeout).Should(gomega.Equal(5))
+		gomega.Expect(result.Success).Should(gomega.BeTrue())
+		gomega.Expect(result.Result).Should(gomega.Equal("rootDirectoryFile2\nsubDirFile\nrootDirectoryFile1\n"))
+	})
 })
